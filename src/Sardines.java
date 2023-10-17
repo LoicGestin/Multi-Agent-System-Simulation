674cc954664d1af6c072e4846ad4e250b0ee5aaa
loic gestin <loic.gestin@grenoble-inp.org>
2023-10-17 11:41:38 +0200

    New rule sardine

--- src/Sardines.java
+++ src/Sardines.java
@@ -10,7 +10,19 @@ public class Sardines extends Essaim{
                 new ArrayList<>(List.of(new String[]{"Requins"})));
     }
 
-
+    private Vector rule6(Boid boid){
+        ArrayList<Boid> voisins = getVoisinsAvecAngle(boid, 300, getDistance());
+        if (voisins.size() == 0){
+            return new Vector(boid.getVitesse().getX() * 1.2, boid.getVitesse().getY() * 1.2);
+        }
+        return new Vector(0,0);
+    }
+    @Override
+    public ArrayList<Vector> rules(Boid boid) {
+        ArrayList<Vector> vectors = super.rules(boid);
+        vectors.add(rule6(boid));
+        return vectors;
+    }
     @Override
     public void setUpEvent(EventManager eventManager) {
         eventManager.addEvent(new ActionEvent(0,eventManager,1,this));
