df25f5a04ac26704a571503147460e4fe44e4c12
florianae <146985324+florianae@users.noreply.github.com>
2023-11-17 17:08:58 +0100

    Update TestGameOfLifeSimulator.java : Coding Style

--- src/TestGameOfLifeSimulator.java
+++ src/TestGameOfLifeSimulator.java
@@ -7,23 +7,23 @@ public class TestGameOfLifeSimulator {
 
         GameOfLife gameOfLife = new GameOfLife(30, 30);
         /*
-        gameOfLife.make_alive(3,1);
-        gameOfLife.make_alive(2,2);
-        gameOfLife.make_alive(2,3);
-        gameOfLife.make_alive(1,2);
+        gameOfLife.makeAlive(3,1);
+        gameOfLife.makeAlive(2,2);
+        gameOfLife.makeAlive(2,3);
+        gameOfLife.makeAlive(1,2);
         */
 
-        gameOfLife.make_alive(5, 5, 1);
-        gameOfLife.make_alive(5, 6, 1);
-        gameOfLife.make_alive(5, 7, 1);
-        gameOfLife.make_alive(4, 6, 1);
+        gameOfLife.makeAlive(5, 5, 1);
+        gameOfLife.makeAlive(5, 6, 1);
+        gameOfLife.makeAlive(5, 7, 1);
+        gameOfLife.makeAlive(4, 6, 1);
 
 
-        gameOfLife.make_alive(7, 8, 1);
-        gameOfLife.make_alive(8, 9, 1);
-        gameOfLife.make_alive(9, 7, 1);
-        gameOfLife.make_alive(9, 8, 1);
-        gameOfLife.make_alive(9, 9, 1);
+        gameOfLife.makeAlive(7, 8, 1);
+        gameOfLife.makeAlive(8, 9, 1);
+        gameOfLife.makeAlive(9, 7, 1);
+        gameOfLife.makeAlive(9, 8, 1);
+        gameOfLife.makeAlive(9, 9, 1);
 
         GUISimulator gui = new GUISimulator(750, 750, Color.BLACK);
         gui.setSimulable(new GameSimulator(gameOfLife, gui));
