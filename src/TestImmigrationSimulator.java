c0357e693c0e50813a0fd86639f68ae37a9cbc6b
Loïc Gestin <loic.gestin34@gmail.com>
2023-11-17 17:48:50 +0100

    Error verification Scheling / Immigration test change

--- src/TestImmigrationSimulator.java
+++ src/TestImmigrationSimulator.java
@@ -4,7 +4,16 @@ import java.awt.*;
 
 public class TestImmigrationSimulator {
     public static void main(String[] args) {
-        Immigration immigration = new Immigration(50, 50, 5);
+        Immigration immigration = new Immigration(80, 80, 5);
+        immigration.fillRandomStates();
+
+        /* Ou vous pouvez rentrez à la main chaque cases / ou vous pouvez les rentrer depuis l'interface avec le click
+        gameOfLife.makeAlive(3,1);
+        gameOfLife.makeAlive(2,2);
+        gameOfLife.makeAlive(2,3);
+        gameOfLife.makeAlive(1,2);
+        */
+
 
 
         GUISimulator gui = new GUISimulator(750, 750, Color.BLACK);
