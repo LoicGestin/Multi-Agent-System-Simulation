d4708c8eefc54a98ccd4a0572f6680d5cd5e92aa
florianae <146985324+florianae@users.noreply.github.com>
2023-11-17 17:10:04 +0100

    Update TestImmigrationSimulator.java : Coding Style

--- src/TestImmigrationSimulator.java
+++ src/TestImmigrationSimulator.java
@@ -6,41 +6,41 @@ public class TestImmigrationSimulator {
     public static void main(String[] args) {
         Immigration immigration = new Immigration(30, 30, 5);
         /*
-        gameOfLife.make_alive(3,1);
-        gameOfLife.make_alive(2,2);
-        gameOfLife.make_alive(2,3);
-        gameOfLife.make_alive(1,2);
+        gameOfLife.makeAlive(3,1);
+        gameOfLife.makeAlive(2,2);
+        gameOfLife.makeAlive(2,3);
+        gameOfLife.makeAlive(1,2);
         */
         immigration.fillRandomStates();
-        immigration.make_alive(4, 5, 1);
-        immigration.make_alive(4, 1 + 5, 1);
+        immigration.makeAlive(4, 5, 1);
+        immigration.makeAlive(4, 1 + 5, 1);
 
-        immigration.make_alive(5, 5, 1);
-        immigration.make_alive(5, 1 + 5, 1);
-        immigration.make_alive(5, 2 + 5, 3);
-        immigration.make_alive(5, 3 + 5, 1);
+        immigration.makeAlive(5, 5, 1);
+        immigration.makeAlive(5, 1 + 5, 1);
+        immigration.makeAlive(5, 2 + 5, 3);
+        immigration.makeAlive(5, 3 + 5, 1);
 
-        immigration.make_alive(1 + 5, 5, 3);
-        immigration.make_alive(1 + 5, 1 + 5, 1);
-        immigration.make_alive(1 + 5, 2 + 5, 3);
-        immigration.make_alive(1 + 5, 3 + 5, 3);
-        immigration.make_alive(1 + 5, 4 + 5, 2);
+        immigration.makeAlive(1 + 5, 5, 3);
+        immigration.makeAlive(1 + 5, 1 + 5, 1);
+        immigration.makeAlive(1 + 5, 2 + 5, 3);
+        immigration.makeAlive(1 + 5, 3 + 5, 3);
+        immigration.makeAlive(1 + 5, 4 + 5, 2);
 
-        immigration.make_alive(2 + 5, 5, 1);
-        immigration.make_alive(2 + 5, 1 + 5, 1);
-        immigration.make_alive(2 + 5, 2 + 5, 3);
-        immigration.make_alive(2 + 5, 3 + 5, 3);
-        immigration.make_alive(2 + 5, 4 + 5, 2);
+        immigration.makeAlive(2 + 5, 5, 1);
+        immigration.makeAlive(2 + 5, 1 + 5, 1);
+        immigration.makeAlive(2 + 5, 2 + 5, 3);
+        immigration.makeAlive(2 + 5, 3 + 5, 3);
+        immigration.makeAlive(2 + 5, 4 + 5, 2);
 
-        immigration.make_alive(3 + 5, 1 + 5, 1);
-        immigration.make_alive(3 + 5, 2 + 5, 2);
-        immigration.make_alive(3 + 5, 3 + 5, 2);
-        immigration.make_alive(3 + 5, 4 + 5, 2);
+        immigration.makeAlive(3 + 5, 1 + 5, 1);
+        immigration.makeAlive(3 + 5, 2 + 5, 2);
+        immigration.makeAlive(3 + 5, 3 + 5, 2);
+        immigration.makeAlive(3 + 5, 4 + 5, 2);
 
-        immigration.make_alive(4 + 5, 1 + 5, 2);
-        immigration.make_alive(4 + 5, 2 + 5, 2);
-        immigration.make_alive(4 + 5, 3 + 5, 2);
-        immigration.make_alive(4 + 5, 4 + 5, 1);
+        immigration.makeAlive(4 + 5, 1 + 5, 2);
+        immigration.makeAlive(4 + 5, 2 + 5, 2);
+        immigration.makeAlive(4 + 5, 3 + 5, 2);
+        immigration.makeAlive(4 + 5, 4 + 5, 1);
 
         GUISimulator gui = new GUISimulator(750, 750, Color.BLACK);
         gui.setSimulable(new GameSimulator(immigration, gui));
